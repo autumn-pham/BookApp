e11a77ca0c9afb2970b2186319891fb34ac32a68
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-10-01 12:20:05 -0700

    updated server and added to controllers

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
-const app = express();
+const mongoose = require ('mongoose');
+require('dotenv').config()
 
-app.get('/books', (req, res)=>{
-  res.send('hello');
+//Configuration
+const app = express();
+const db = mongoose.connection;
+
+//PORT
+const PORT = process.env.PORT || 3001;
+
+//DATABASE
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
 
+// Error / success
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 
 // /MIDDLEWARE
 app.use(express.json()) //use .json(), not .urlencoded()
+app.use(express.static('public'))
+
+//ROUTES
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
+
+
 
 // LISTENER
-app.listen(3001, () => {
-  console.log('listening on port')
-})
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
